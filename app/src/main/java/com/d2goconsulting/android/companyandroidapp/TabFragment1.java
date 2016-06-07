4ca753d8280b003ca829e33d618ff4f529b7037f
d2goconsulting <d2goconsulting@gmail.com>
2016-06-06 23:15:33 -0700

    android app
    android app 2016-06-06

---
+++ app/src/main/java/com/d2goconsulting/android/companyandroidapp/TabFragment1.java
@@ -0,0 +1,20 @@
+package com.d2goconsulting.android.companyandroidapp;
+
+/**
+ * Created by Ophelia on 2016-06-06.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.d2goconsulting.android.companyandroidapp.R;
+
+public class TabFragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+    }
+}
